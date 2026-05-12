0942f951ea05d53766ddf35e676ad55930d41f7c
fannybillefalt <fannybillefalt@gmail.com>
2026-05-12 14:09:59 +0200

    PairCoding-Created props in Flight model

--- SaddleHeroesAirWays.Library/Models/Flight.cs
+++ SaddleHeroesAirWays.Library/Models/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace SaddleHeroesAirWays.Library.Models
 {
     internal class Flight
     {
+        public int Id { get; set; }
+        public string? FlightNumber { get; set; }
+        public int DepartureAirportId { get; set; }
+        public int ArrivalAirportId { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+        public int TotalSeats { get; set; }
+        public decimal Price { get; set; }
+        public Status FlightStatus { get; set; }
+    }
+
+    public enum Status
+    {
+        Arrived, 
+        Delayed, 
+        Cancelled
     }
 }
