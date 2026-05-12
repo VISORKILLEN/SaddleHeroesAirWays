624ea2864d6724417a804b60bac14ada9a96610b
fannybillefalt <fannybillefalt@gmail.com>
2026-05-12 14:43:01 +0200

    PairCoding-added dataannotations (inverseProperty) in airport model

--- SaddleHeroesAirWays.Library/Models/Airport.cs
+++ SaddleHeroesAirWays.Library/Models/Airport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,11 @@ namespace SaddleHeroesAirWays.Library.Models
         public string? Name { get; set; }
         public string? City { get; set; }
         public string? Country { get; set; }
+
+        [InverseProperty("DepartureAirport")]
         public ICollection<Flight>? DepartingFlights { get; set; }
+
+        [InverseProperty("ArrivalAirport")]
         public ICollection<Flight>? ArrivingFlights { get; set; }
     }
 }
