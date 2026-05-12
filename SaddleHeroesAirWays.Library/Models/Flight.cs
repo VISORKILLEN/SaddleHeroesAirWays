d28370903e592d71fa2992c6601fff9c347e1bdb
fannybillefalt <fannybillefalt@gmail.com>
2026-05-12 14:41:20 +0200

    PairCoding-Added data annotations (inverseproperty and FK) in Flight

--- SaddleHeroesAirWays.Library/Models/Flight.cs
+++ SaddleHeroesAirWays.Library/Models/Flight.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,13 @@ namespace SaddleHeroesAirWays.Library.Models
         public FlightStatus FlightStatus { get; set; }
 
         public ICollection<Booking>? Bookings { get; set; }
+
+        [ForeignKey("DepartureAirportId")]
+        [InverseProperty("DepartingFlights")]
         public Airport? DepartureAirport { get; set; }
+
+        [ForeignKey("ArrivalAirportId")]
+        [InverseProperty("ArrivingFlights")]
         public Airport? ArrivalAirport { get; set; }
     }
 
