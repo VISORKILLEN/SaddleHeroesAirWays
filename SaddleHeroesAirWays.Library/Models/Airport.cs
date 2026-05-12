ecb584bc61a631fbecacc9c768016c6bfe5e9ffe
fannybillefalt <fannybillefalt@gmail.com>
2026-05-12 13:29:51 +0200

    PairCoding-Created props in Airport model

--- SaddleHeroesAirWays.Library/Models/Airport.cs
+++ SaddleHeroesAirWays.Library/Models/Airport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace SaddleHeroesAirWays.Library.Models
 {
     internal class Airport
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string? IATACode { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
     }
 }
