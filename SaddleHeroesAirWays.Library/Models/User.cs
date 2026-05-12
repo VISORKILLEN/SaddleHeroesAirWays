0261b351e1bde72ea9d686fc6fd780e20d9de2bf
fannybillefalt <fannybillefalt@gmail.com>
2026-05-12 14:13:40 +0200

    PairCoding-Created props in User model

--- SaddleHeroesAirWays.Library/Models/User.cs
+++ SaddleHeroesAirWays.Library/Models/User.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace SaddleHeroesAirWays.Library.Models
 {
     internal class User
     {
+        public int Id { get; set; }
+        public string? Gender { get; set; }
+        public string? Firstname { get; set; }
+
+        public string? Lastname { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? Phonenumber { get; set; }
+        public string? SocialSecurityNumber { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
