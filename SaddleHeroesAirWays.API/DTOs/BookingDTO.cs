ec242bb63e264e2767f333186461e0d04f5ee5d3
fannybillefalt <fannybillefalt@gmail.com>
2026-05-14 16:19:44 +0200

    added bookingresponse, createbooking DTOs

--- SaddleHeroesAirWays.API/DTOs/BookingDTO.cs
+++ SaddleHeroesAirWays.API/DTOs/BookingDTO.cs
@@ -1,6 +1,5 @@
 ﻿namespace SaddleHeroesAirWays.API.DTOs
 {
-    public class BookingDTO
-    {
-    }
+    public record BookingResponse(string? BookingReference, string? Firstname, string? Lastname, string? Flightnumber, string? DepartureAirport, string? ArrivalAirport, DateTime DepartureTime, DateTime BookingDate, decimal TotalPrice, string? BookingStatus);
+    public record CreateBooking(int UserId, int FlightId);
 }
