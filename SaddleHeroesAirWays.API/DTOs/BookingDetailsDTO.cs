3bcdcad31a59a4c1f565b1c93e04b2fd01f436f5
fannybillefalt <fannybillefalt@gmail.com>
2026-05-14 16:20:18 +0200

    added bookingdetailsresponse, createbookingdetails DTOs

---
+++ SaddleHeroesAirWays.API/DTOs/BookingDetailsDTO.cs
@@ -0,0 +1,5 @@
+﻿namespace SaddleHeroesAirWays.API.DTOs
+{
+    public record BookingDetailsResponse(int Id, string? SeatNumber, bool Baggage, string? Notes);
+    public record CreateBookingDetails(string? SeatNumber, bool Baggage, string? Notes);
+}
