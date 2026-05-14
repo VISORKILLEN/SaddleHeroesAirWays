c110678e97a88253924a1d9b4474fc0d486fd898
fannybillefalt <fannybillefalt@gmail.com>
2026-05-14 16:18:44 +0200

    added userrespone, createuser, updateuser DTOs

--- SaddleHeroesAirWays.API/DTOs/UserDTO.cs
+++ SaddleHeroesAirWays.API/DTOs/UserDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace SaddleHeroesAirWays.API.DTOs
 {
-    public class UserDTO
-    {
-    }
+    public record UserResponse(int Id, string? Firstname, string? Lastname, string? Email, string? Phonenumber);
+    public record CreateUser(string? Gender, string? Firstname, string? Lastname, string? Email, string? Phonenumber, string SocialSecurityNumber);
+    public record UpdateUser(string? FIrstname, string? Lastname, string? Email, string? Phonenumber);
 }
