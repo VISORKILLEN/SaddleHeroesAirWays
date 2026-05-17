4a00719a617143b78972764c020c65c59c573ded
VISORKILLEN <viktor.gustavsson@live.se>
2026-05-17 21:34:20 +0200

    fixed in GetFligthsforWeek so it works better on certain days//Viktor

--- SaddleHeroesAirWays.Library/Services/FlightServices.cs
+++ SaddleHeroesAirWays.Library/Services/FlightServices.cs
@@ -6,20 +6,22 @@ namespace SaddleHeroesAirWays.Library.Services
     {
         private readonly List<Flight> _flights;
 
-
         public FlightServices(List<Flight> flights)
         {
             _flights = flights;
         }
 
+        // This method retrieves all flights that are scheduled to depart within the week of the provided date.
         public List<Flight> GetFlightsForWeek(DateTime date)
         {
-            var startOfWeek = date.AddDays(-(int)date.DayOfWeek + 1);
+            int diff = (7 + (date.DayOfWeek - DayOfWeek.Monday)) % 7;
+
+            var startOfWeek = date.AddDays(-1 * diff).Date;
             var endOfWeek = startOfWeek.AddDays(7);
 
             return _flights
                 .Where(f => f.DepartureTime >= startOfWeek &&
-                f.DepartureTime < endOfWeek)
+                                  f.DepartureTime < endOfWeek)
                 .ToList();
         }
 
