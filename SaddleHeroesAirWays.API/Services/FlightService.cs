4d2752028e9dc7061b533738a0a808ad0535b455
VISORKILLEN <viktor.gustavsson@live.se>
2026-05-18 20:10:04 +0200

    Improved GetFlightsWeek with DBContext//Viktor

--- SaddleHeroesAirWays.API/Services/FlightService.cs
+++ SaddleHeroesAirWays.API/Services/FlightService.cs
@@ -1,28 +1,28 @@
-﻿using SaddleHeroesAirWays.Library.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SaddleHeroesAirWays.Library.Models;
 
 namespace SaddleHeroesAirWays.API.Services
 {
     public class FlightService
     {
-        private readonly List<Flight> _flights;
+        private readonly DbContextAPI _context;
 
-        public FlightService(List<Flight> flights)
+        public FlightService(DbContextAPI context)
         {
-            _flights = flights;
+            _context = context;
         }
 
-        // This method retrieves all flights that are scheduled to depart within the week of the provided date.
-        public List<Flight> GetFlightsForWeek(DateTime date)
+        public async Task<List<Flight>> GetFlightsForWeekAsync(DateTime date)
         {
             int diff = (7 + (date.DayOfWeek - DayOfWeek.Monday)) % 7;
 
-            var startOfWeek = date.AddDays(-1 * diff).Date;
+            var startOfWeek = date.AddDays(-diff).Date;
             var endOfWeek = startOfWeek.AddDays(7);
 
-            return _flights
+            return await _context.Flight
                 .Where(f => f.DepartureTime >= startOfWeek &&
-                                  f.DepartureTime < endOfWeek)
-                .ToList();
+                            f.DepartureTime < endOfWeek)
+                .ToListAsync();
         }
     }
 }
