6fd46e25d876e92704c83d5013bbb7ed0eaa7d2a
simonFCO <simon.vare6@gmail.com>
2026-05-18 10:05:13 +0200

    Endpoint using Dto Setup complete

--- SaddleHeroesAirWays.API/Controllers/UserController.cs
+++ SaddleHeroesAirWays.API/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaddleHeroesAirWays.API.DTOs;
 using SaddleHeroesAirWays.API.Services;
 using SaddleHeroesAirWays.API.Services.Interfaces;
+using SaddleHeroesAirWays.Library.Models;
 
 namespace SaddleHeroesAirWays.API.Controllers
 {
@@ -13,6 +15,13 @@ namespace SaddleHeroesAirWays.API.Controllers
         public UserController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpPost("/CreateUser")]
+        public async Task<ActionResult<IEnumerable<CreateUser>>> GetAllPersons(int id, string FirstName, string LastName, string Gender, string Email, string PhoneNumber, string SocialSecurityNumber, bool IsAdmin)
+        {
+            User NewUser = new User { Id = id, Firstname = FirstName, Lastname = LastName, Gender = Gender, Email = Email, Phonenumber = PhoneNumber, SocialSecurityNumber = SocialSecurityNumber, IsAdmin = IsAdmin};
+            return Ok(NewUser);
         }
     }
 }
