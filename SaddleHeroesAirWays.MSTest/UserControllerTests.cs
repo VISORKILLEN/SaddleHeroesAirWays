f010fee73f01b46ea98f43a1b960a22ccbc9a078
simonFCO <simon.vare6@gmail.com>
2026-05-20 16:23:39 +0200

    UserControllerTests setup complete

---
+++ SaddleHeroesAirWays.MSTest/UserControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using SaddleHeroesAirWays.API.Controllers;
+using SaddleHeroesAirWays.API.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaddleHeroesAirWays.MSTest
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        private Mock<IUserService> _UserService;
+
+        private UserController _UserController;
+
+        public UserControllerTests()
+        {
+            _UserService = new Mock<IUserService>();
+        }
+    }
+}
