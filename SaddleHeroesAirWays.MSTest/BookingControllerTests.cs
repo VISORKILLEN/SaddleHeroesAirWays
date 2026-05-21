9e10e03102ff8ae0a51bf9acc3b61358a435471e
fannybillefalt <fannybillefalt@gmail.com>
2026-05-21 23:04:48 +0200

    Created happy path moq test for bookingcontroller

--- SaddleHeroesAirWays.MSTest/BookingControllerTests.cs
+++ SaddleHeroesAirWays.MSTest/BookingControllerTests.cs
@@ -56,6 +56,21 @@ namespace SaddleHeroesAirWays.MSTest
         }
 
         [TestMethod]
-        public async Task GetBookingsByUserIdAsync__ReturnsSuccess
+        public async Task GetBookingsByUserId_ValidUserId_ReturnsOk()
+        {
+            var userId = 1;
+            _mockBookingService
+                .Setup(s => s.GetBookingsByUserIdAsync(userId))
+                .ReturnsAsync(new List<BookingResponse>
+                {
+                    new BookingResponse("BKG-001", "Arthur", "Morgan", "SH-101", "Stockholm Arlanda", "Heathrow Airport", new DateTime(2026, 6, 1), new DateTime(2026, 5, 1), 150m, "Confirmed", null)
+                });
+
+            var actual = await _controller.GetBookingsByUserId(userId);
+
+            var okResult = actual.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult.StatusCode);
+        }
     }
 }
