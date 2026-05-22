7da54be9020d0041649964afc2b13975ecc6c346
fannybillefalt <fannybillefalt@gmail.com>
2026-05-22 12:52:56 +0200

    Created CreateBookingAsync in BookingService

--- SaddleHeroesAirWays.API/Services/BookingService.cs
+++ SaddleHeroesAirWays.API/Services/BookingService.cs
@@ -98,7 +98,54 @@ namespace SaddleHeroesAirWays.API.Services
                 .ToListAsync();
         }
 
+        public async Task<BookingResponse> CreateBookingAsync(CreateBookingRequest bookingRequest)
+        {
+            var flight = await _context.Flight.FindAsync(bookingRequest.FlightId);
+            
+            if(flight == null)
+            {
+                return null;
+            }
 
+            var count = await _context.Booking.CountAsync();
+            
+            var bookingToAdd = new Booking
+            {
+                UserId = bookingRequest.UserId,
+                FlightId = bookingRequest.FlightId,
+                BookingDate = DateTime.Now,
+                BookingStatus = BookingStatus.Confirmed,
+                TotalPrice = flight.Price,
+                BookingReference = $"BKG-{1000 + count + 1}"
+            };
+
+            await _context.AddAsync(bookingToAdd);
+            await _context.SaveChangesAsync();
+
+            var createdBooking = await _context.Booking
+                .Where(b => b.BookingReference == bookingToAdd.BookingReference)
+                .Select(b => new BookingResponse(
+                    b.BookingReference,
+                    b.User.Firstname,
+                    b.User.Lastname,
+                    b.Flight.FlightNumber,
+                    b.Flight.DepartureAirport.Name,
+                    b.Flight.ArrivalAirport.Name,
+                    b.Flight.DepartureTime,
+                    b.BookingDate,
+                    b.TotalPrice,
+                    b.BookingStatus.ToString(),
+                    b.BookingDetails.Select(bd => new BookingDetailsResponse(
+                        bd.Id,
+                        bd.Seatnumber,
+                        bd.Baggage,
+                        bd.Notes
+                        ))
+                ))
+                .FirstOrDefaultAsync();
+
+            return createdBooking;
+        }
 
     }
 }
