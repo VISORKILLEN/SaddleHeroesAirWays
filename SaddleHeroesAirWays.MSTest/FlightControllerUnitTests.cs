0612770deff048a9c7cbf9656bffd2c28e8cb3fe
VISORKILLEN <viktor.gustavsson@live.se>
2026-05-27 11:59:01 +0200

    made first happytest any city for api endpoint//Viktor

--- SaddleHeroesAirWays.MSTest/FlightControllerUnitTests.cs
+++ SaddleHeroesAirWays.MSTest/FlightControllerUnitTests.cs
@@ -1,5 +1,7 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using SaddleHeroesAirWays.API.Controllers;
+using SaddleHeroesAirWays.API.DTOs;
 using SaddleHeroesAirWays.API.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,7 +12,7 @@ using System.Threading.Tasks;
 namespace SaddleHeroesAirWays.MSTest
 {
     [TestClass]
-    internal class FlightControllerUnitTests
+    public class FlightControllerUnitTests
     {
         private readonly Mock<IFlightService> _mockFlightService;
         private readonly FlightController _controller;
@@ -19,6 +21,23 @@ namespace SaddleHeroesAirWays.MSTest
         {
             _mockFlightService = new Mock<IFlightService>();
             _controller = new FlightController(_mockFlightService.Object);
+        }
+
+        // Happy path test, verifies that the controller returns OK with available flights
+        public async Task SearchAvailableFlights_NoCity_ReturnOK()
+        {
+            _mockFlightService
+                .Setup(s => s.SearchAvailableFlightsAsync(null))
+                .ReturnsAsync(new List<FlightResponse>
+                {
+                    new FlightResponse("SH001", "Stockholm Arlanda", "Heathrow Airport", new DateTime(2026, 6, 1), new DateTime(2026, 6, 1), 149, 150m, "Arrived")
+                });
+
+            var result = await _controller.SearchAvailableFlights(null);
+
+            var ok = result as OkObjectResult;
+            Assert.IsNotNull(ok);
+            Assert.AreEqual(200, ok.StatusCode);
         }
     }
 }
