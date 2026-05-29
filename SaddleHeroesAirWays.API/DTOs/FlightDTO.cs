802bea4f562e72caec0f8b4f39ccbfd7d08231f5
fannybillefalt <fannybillefalt@gmail.com>
2026-05-29 16:51:45 +0200

    changed from string flightstatus to enum flightenum in flightresponse dto

--- SaddleHeroesAirWays.API/DTOs/FlightDTO.cs
+++ SaddleHeroesAirWays.API/DTOs/FlightDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace SaddleHeroesAirWays.API.DTOs
 {
-    public record FlightResponse(string Flightnumber, string DepartureAirport, string ArrivalAirport, DateTime DepartureTime, DateTime ArrivalTime, int TotalSeats, decimal Price, string? FlightStatus);
+    public record FlightResponse(int Id, string Flightnumber, string DepartureAirport, string ArrivalAirport, DateTime DepartureTime, DateTime ArrivalTime, int TotalSeats, decimal Price, Enum FlightStatus);
 
     public record UpdateFlight(DateTime DepartureTime, DateTime ArrivalTime, decimal Price, string? FlightStatus);
 }
