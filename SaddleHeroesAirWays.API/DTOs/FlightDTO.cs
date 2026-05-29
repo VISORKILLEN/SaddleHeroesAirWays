66143ca1ba30558e3a2102de9ab90b5544e3181c
fannybillefalt <fannybillefalt@gmail.com>
2026-05-29 16:14:41 +0200

    added a getflightbyid interface

--- SaddleHeroesAirWays.API/DTOs/FlightDTO.cs
+++ SaddleHeroesAirWays.API/DTOs/FlightDTO.cs
@@ -2,7 +2,5 @@
 {
     public record FlightResponse(string Flightnumber, string DepartureAirport, string ArrivalAirport, DateTime DepartureTime, DateTime ArrivalTime, int TotalSeats, decimal Price, string? FlightStatus);
 
-    //behövs en createFlight?
-
     public record UpdateFlight(DateTime DepartureTime, DateTime ArrivalTime, decimal Price, string? FlightStatus);
 }
